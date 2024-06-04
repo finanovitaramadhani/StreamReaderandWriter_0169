bddce136b1ccb8e15d52158b3253feadc038167b
finanovita <finanovita479@gmail.com>
2024-06-04 11:16:10 +0700

    tray

---
+++ exeptionStandard/exeptionStandard.cpp
@@ -0,0 +1,25 @@
+// exeptionStandard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan 
+#include <array>
+//untuk onjek array yang akan kita gunakan 
+using namespace std;
+
+
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array onteger 3 elemen 
+		//cout << data.at(5) << endl;
+		//memanggil array elemen ke 5 
+	}
+
+	
+}
+
+
