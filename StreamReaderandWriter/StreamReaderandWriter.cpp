8f20ea95dc201c8a1c2d9ef955a7aa6a92bd5d32
finanovita <finanovita479@gmail.com>
2024-06-04 10:26:20 +0700

    Add project files.

---
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -0,0 +1,14 @@
+// StreamReaderandWriter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   
+}
+
+
